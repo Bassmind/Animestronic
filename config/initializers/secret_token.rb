b2428456742c56f174c08d116cbe03efa177b3bc
David <isc.david.mendiola@hotmail.com>
2012-11-07 19:00:47 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Animestronic::Application.config.secret_token = 'f819b7da17ddca814fb11881553b6d1468de7d4aeef6f497f39318a695f60baec30d1a7f031797079ff258604961daf8f2a2f718cec9207aadb2390706249abd'
